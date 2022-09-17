9bc999aafaa7b5a9e0e877bf004490f7d9497c1b
emmaran <emmalrandolph@gmail.com>
2022-09-17 00:19:13 -0500

    ADDED FORM - SUBMITS TO GREETING

--- src/main/java/org/launchcode/hellospring/controllers/HelloController.java
+++ src/main/java/org/launchcode/hellospring/controllers/HelloController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.*;
  */
 @Controller
 @ResponseBody
-@RequestMapping("hello")
 public class HelloController {
 
     //    // Handle request at path /hello
@@ -18,35 +17,89 @@ public class HelloController {
 //        return "Hello, Spring!";
 //    }
 
-    // lives at /hello/goodbye
-    @GetMapping("goodbye")
-    public String goodbye() {
-        return "Goodbye, Spring!";
-    }
-
-    // Handles requests of the form /hello?name=LaunchCode
-    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
-    public String helloWithQueryParam(@RequestParam String name) {
-        return "Hello, " + name + "!";
-    }
-
-    // Handles requests of the form /hello/LaunchCode
-    @GetMapping("{name}")
-    public String helloWithPathParam(@PathVariable String name) {
-        return "Hello, " + name + "!";
-    }
+//    // lives at /hello/goodbye
+//    @GetMapping("goodbye")
+//    public String goodbye() {
+//        return "Goodbye, Spring!";
+//    }
+//
+//    // Handles requests of the form /hello?name=LaunchCode
+//    @RequestMapping(method = {RequestMethod.GET, RequestMethod.POST})
+//    public String helloWithQueryParam(@RequestParam String name) {
+//        return "Hello, " + name + "!";
+//    }
+//
+//    // Handles requests of the form /hello/LaunchCode
+//    @GetMapping("{name}")
+//    public String helloWithPathParam(@PathVariable String name) {
+//        return "Hello, " + name + "!";
+//    }
 
     // /hello/form
+//    @GetMapping("form")
+//    public String helloForm() {
+//        return "<html>" +
+//                "<body>" +
+//                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+//                "<input type = 'text' name = 'name' >" +
+//                "<input type = 'submit' value = 'Greet Me!' >" +
+//                "</form>" +
+//                "</body>" +
+//                "</html>";
+//    }
+
+    @RequestMapping(value="hello", method = RequestMethod.POST)
+    public String helloPost(@RequestParam String name, @RequestParam String language) {
+//        return "Hello, " + name + "!";
+        if (name == null) {
+            name = "World";
+        }
+
+        return "<html>" +
+                "<h1>" +
+                createMessage(name, language) +
+                "</h1>" +
+                "</html>";
+
+    }
+
+    public static String createMessage(String n, String l) {
+        String greeting = "";
+
+        if (l.equals("english")) {
+            greeting = "Hello";
+        } else if (l.equals("spanish")) {
+            greeting = "Hola";
+        } else if (l.equals("french")) {
+            greeting = "Bonjour";
+        } else if (l.equals("german")) {
+            greeting = "Hallo";
+        } else if (l.equals("italian")) {
+            greeting = "Bonjourno";
+        }
+
+        return greeting + ", " + n + "!";
+    }
+
     @GetMapping("form")
     public String helloForm() {
         return "<html>" +
                 "<body>" +
-                "<form action = '/hello' method = 'post'>" + // submit a request to /hello
+                "<form action = '/hello' method = 'post'>" +
                 "<input type = 'text' name = 'name' >" +
+                "<select name = 'language' >" +
+                "<option value = 'english' >English</option>" +
+                "<option value = 'spanish' >Spanish</option>" +
+                "<option value = 'french' >French</option>" +
+                "<option value = 'german' >German</option>" +
+                "<option value = 'italian' >Italian</option>" +
+                "</select>" +
                 "<input type = 'submit' value = 'Greet Me!' >" +
                 "</form>" +
                 "</body>" +
                 "</html>";
     }
+
+
 
 }
